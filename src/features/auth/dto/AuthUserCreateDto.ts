e5ba11e6f7d1d9dae730341148a1be53fe9efd1f
taras <regeidt.my@gmail.com>
2023-10-04 14:39:00 +0300

    fix

--- src/features/auth/dto/AuthUserCreateDto.ts
+++ src/features/auth/dto/AuthUserCreateDto.ts
@@ -1,13 +1,17 @@
 import { IsNotEmptyString } from '../../../application/decorators/validation/IsNotEmptyString';
 import { IsEmail, MaxLength, MinLength } from 'class-validator';
 import { LOGIN_MAX, LOGIN_MIN, PASSWORD_MAX, PASSWORD_MIN } from './dto.variables';
+import { IsUniqueUserLogin } from '../../../application/decorators/validation/IsUniqueUserLogin';
+import { IsUniqueUserEmail } from '../../../application/decorators/validation/IsUniqueUserEmail';
 
 export class AuthUserCreateDto {
+  @IsUniqueUserLogin()
   @IsNotEmptyString()
   @MinLength(LOGIN_MIN)
   @MaxLength(LOGIN_MAX)
   login: string;
 
+  @IsUniqueUserEmail()
   @IsNotEmptyString()
   @IsEmail()
   email: string;
