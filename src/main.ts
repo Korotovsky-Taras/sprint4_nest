0ce046f6041b097e4b7838e6d76b552d7d4fe423
taras <regeidt.my@gmail.com>
2023-10-11 20:14:34 +0300

    - winston logs

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useAppSettings } from './application/utils/useAppSettings';
-import { WinstonModule } from 'nest-winston';
-import { format, transports } from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        // let's log errors into its own file
-        new transports.File({
-          filename: `logs/error.log`,
-          level: 'error',
-          format: format.combine(format.timestamp(), format.json()),
-        }),
-        // logging all level
-        new transports.File({
-          filename: `logs/combined.log`,
-          format: format.combine(format.timestamp(), format.json()),
-        }),
-        // we also want to see logs in our console
-        new transports.Console({
-          format: format.combine(
-            format.cli(),
-            format.splat(),
-            format.timestamp(),
-            format.printf((info) => {
-              return `${info.timestamp} ${info.level}: ${info.message}`;
-            }),
-          ),
-        }),
-      ],
-    }),
-  });
+  const app = await NestFactory.create(AppModule);
   useAppSettings(app);
   await app.listen(3000);
 }
