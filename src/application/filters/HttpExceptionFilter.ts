af5e8e684e3754d34d245444886c88a78660fb3c
taras <regeidt.my@gmail.com>
2024-01-23 16:19:45 +0300

    p_25
    devMode logging validation error

--- src/application/filters/HttpExceptionFilter.ts
+++ src/application/filters/HttpExceptionFilter.ts
@@ -1,6 +1,8 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { ApiValidationError } from '../core/ApiValidationError';
+import { ConfigService } from '@nestjs/config';
+import { AppConfiguration, AppDbType } from '../utils/config';
 
 @Catch(ApiValidationError)
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -8,6 +10,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
+    const request = ctx.getRequest<Request>();
+    const body = request.body;
+    const query = request.query;
+
+    const configService = new ConfigService<AppConfiguration, true>();
+    const isDevMode = configService.get<AppDbType>('DEV_MODE');
+
+    if (isDevMode) {
+      console.log(`[body] ${JSON.stringify(body)}, [query] ${JSON.stringify(query)},`);
+    }
 
     response.status(status).send({
       errorsMessages: exception.errors,
